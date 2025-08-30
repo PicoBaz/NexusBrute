f739fd8c76822bd85585e3deae0a7967b6006fd9
mehdi <picobaz3@gmail.com>
2025-08-30 10:53:56 +0330

    complete [proxy_rotator new module]

---
+++ modules/proxy_rotator.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const sessionLogger = require('./session_logger');
+
+class ProxyRotator {
+    constructor(proxies) {
+        this.proxies = proxies;
+        this.currentIndex = 0;
+        this.failedProxies = new Set();
+    }
+
+    getNextProxy() {
+        if (this.proxies.length === 0) return null;
+        let attempts = 0;
+        while (attempts < this.proxies.length) {
+            const proxy = this.proxies[this.currentIndex];
+            this.currentIndex = (this.currentIndex + 1) % this.proxies.length;
+            if (!this.failedProxies.has(proxy.host + ':' + proxy.port)) {
+                return proxy;
+            }
+            attempts++;
+        }
+        return null;
+    }
+
+    markProxyFailed(proxy) {
+        this.failedProxies.add(proxy.host + ':' + proxy.port);
+        console.log(chalk.yellow(`[WARN] Proxy failed: ${proxy.host}:${proxy.port}`));
+    }
+
+    createAxiosInstance() {
+        const proxy = this.getNextProxy();
+        if (!proxy) {
+            console.log(chalk.red('[ERROR] No available proxies'));
+            return axios;
+        }
+        console.log(chalk.cyan(`[PROXY] Using ${proxy.protocol}://${proxy.host}:${proxy.port}`));
+        return axios.create({
+            proxy: {
+                host: proxy.host,
+                port: proxy.port,
+                protocol: proxy.protocol
+            }
+        });
+    }
+
+    async createAxiosWithRotation(config, sessionLog) {
+        const axiosInstance = this.createAxiosInstance();
+        sessionLog.push(await sessionLogger.log(config, {
+            operation: 'proxy_rotator',
+            details: `Selected proxy: ${axiosInstance.defaults.proxy?.host}:${axiosInstance.defaults.proxy?.port}`
+        }));
+        return axiosInstance;
+    }
+}
+
+module.exports = { ProxyRotator };
